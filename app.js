3cfca13f4add4bc2aab42f965638890d47bb74bd
Your Name <tomkhughes@g.cofc.edu>
2017-12-18 11:04:22 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAoQnFibZpzlg0n5mJRNe8y-52dL_BhdGM",
+    authDomain: "train-schedule-1c7f0.firebaseapp.com",
+    databaseURL: "https://train-schedule-1c7f0.firebaseio.com",
+    projectId: "train-schedule-1c7f0",
+    storageBucket: "",
+    messagingSenderId: "550558530122"
+  };
+  firebase.initializeApp(config);
+
